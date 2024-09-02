cc11ac71abd8fdbdbe6385a95ae96a8ebe8bbfff
Anton Voytenko <avoy18@gmail.com>
2024-09-02 17:49:10 -0400

    move setup to a class

---
+++ inc/class-petitioner-setup.php
@@ -0,0 +1,84 @@
+<?php
+
+if (! defined('ABSPATH')) {
+    exit; // Exit if accessed directly.
+}
+
+class Petition_Setup
+{
+    public function __construct()
+    {
+        // assets
+        add_action('admin_enqueue_scripts', array($this, 'enqueue_admin_assets'));
+        add_action('wp_enqueue_scripts',  array($this, 'enqueue_frontend_assets'));
+
+        // config hooks
+        register_activation_hook(__FILE__, array($this, 'plugin_activation'));
+        register_deactivation_hook(__FILE__, array($this, 'plugin_deactivate'));
+        register_uninstall_hook(__FILE__, array($this, 'plugin_uninstall'));
+
+        // cpt
+        add_action('init', array($this, 'register_post_types'));
+    }
+
+    /**
+     * Plugin activation callback.
+     */
+    public function plugin_activation()
+    {
+        add_option('petitioner_plugin_version', PTR_VERSION);
+    }
+
+    /**
+     * Plugin deactivation callback.
+     */
+    public function plugin_deactivate()
+    {
+        flush_rewrite_rules();
+    }
+
+    /**
+     * Plugin uninstall callback.
+     */
+    public function plugin_uninstall()
+    {
+        delete_option('petitioner_plugin_version');
+    }
+
+    /**
+     * Register custom post types for the plugin.
+     */
+    public function register_post_types()
+    {
+        register_post_type('petitioner-petition', array(
+            'public'       => true,
+            'label'        => __('Petitioner Petitions', 'petitioner-wp'),
+            'supports'     => array('title'),
+            'has_archive'  => true,
+        ));
+
+        register_post_type('petitioner-entries', array(
+            'public'       => true,
+            'label'        => __('Petitioner submissions', 'petitioner-wp'),
+            'supports'     => array('title'),
+            'has_archive'  => true,
+        ));
+    }
+
+    /**
+     * Enqueue plugin scripts and styles.
+     */
+    public function enqueue_frontend_assets()
+    {
+        wp_enqueue_style('petitioner-style', plugin_dir_url(__FILE__) . 'css/petitioner.css', array(), PTR_VERSION);
+        wp_enqueue_script('petitioner-script', plugin_dir_url(__FILE__) . 'js/petitioner.js', array(), PTR_VERSION, true);
+    }
+    
+    /**
+     * Enqueue admin scripts and styles.
+     */
+    public function enqueue_admin_assets()
+    {
+        wp_enqueue_style('petitioner-css', PTR_PLUGIN_DIR . '/petitioner.css', array(), PTR_VERSION);
+    }
+}
