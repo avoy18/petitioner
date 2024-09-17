1351cb175bbccaaf9ba5c0de56e520f562d0be79
Anton Voytenko <avoy18@gmail.com>
2024-09-17 19:27:00 -0400

    adjust the dev & build process

--- dist/gutenberg/petitionerFormBlock.asset.php
+++ dist/gutenberg/petitionerFormBlock.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => '3314500f9bd1eb21c5e7');
+<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '8f386837b074758e327e');
--- dist/gutenberg/petitionerFormBlock.js
+++ dist/gutenberg/petitionerFormBlock.js
@@ -1,106 +1,13 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/gutenberg/form/Edit.jsx":
 /*!*************************************!*\
   !*** ./src/gutenberg/form/Edit.jsx ***!
   \*************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-__webpack_require__.r(__webpack_exports__);
-/* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "default": () => (/* binding */ Edit)
-/* harmony export */ });
-/* harmony import */ var _wordpress_block_editor__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @wordpress/block-editor */ "@wordpress/block-editor");
-/* harmony import */ var _wordpress_block_editor__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _wordpress_server_side_render__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @wordpress/server-side-render */ "@wordpress/server-side-render");
-/* harmony import */ var _wordpress_server_side_render__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_wordpress_server_side_render__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @wordpress/data */ "@wordpress/data");
-/* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_wordpress_data__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @wordpress/components */ "@wordpress/components");
-/* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__);
-
-
-
-
-
-var continents = ['Africa', 'America', 'Antarctica', 'Asia', 'Europe', 'Oceania'];
-function Edit(_ref) {
-  var attributes = _ref.attributes,
-    setAttributes = _ref.setAttributes;
-  var formId = attributes.formId,
-    newPetitionLink = attributes.newPetitionLink;
-  var blockAtts = (0,_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_0__.useBlockProps)();
-  var allPetitions = (0,_wordpress_data__WEBPACK_IMPORTED_MODULE_2__.useSelect)(function (select) {
-    return select('core').getEntityRecords('postType', 'petitioner-petition', {
-      per_page: -1,
-      _fields: 'title,id'
-    });
-  }, []) || [];
-  var petitionOptions = [];
-  allPetitions.forEach(function (el) {
-    var label = el.title.raw;
-    var limit = 40;
-    if (label.length > limit) {
-      label = label.substring(0, limit) + '...';
-    }
-    var objToPush = {
-      label: label,
-      value: el.id
-    };
-    petitionOptions.push(objToPush);
-  });
-  var FinalCompontnt = function FinalCompontnt() {
-    if (!allPetitions.length) {
-      return /*#__PURE__*/React.createElement("div", {
-        style: {
-          padding: '24px',
-          background: '#efefef'
-        }
-      }, /*#__PURE__*/React.createElement("p", null, " ", /*#__PURE__*/React.createElement("small", null, "Petitioner form")), /*#__PURE__*/React.createElement("p", null, "Looks like you haven't added any forms yet!"), /*#__PURE__*/React.createElement(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.Button, {
-        variant: "secondary",
-        href: newPetitionLink || ''
-      }, "Create your first petition"));
-    }
-    if (!formId) {
-      return /*#__PURE__*/React.createElement("div", {
-        style: {
-          padding: '24px',
-          background: '#efefef'
-        }
-      }, /*#__PURE__*/React.createElement("p", null, " ", /*#__PURE__*/React.createElement("small", null, "Petitioner form")), /*#__PURE__*/React.createElement("p", null, "Use the dropdown in the block settings to select your petiton."));
-    }
-    return /*#__PURE__*/React.createElement("div", {
-      style: {
-        pointerEvents: "none"
-      }
-    }, /*#__PURE__*/React.createElement((_wordpress_server_side_render__WEBPACK_IMPORTED_MODULE_1___default()), {
-      block: "petitioner/form",
-      attributes: attributes
-    }));
-  };
-  return /*#__PURE__*/React.createElement("div", blockAtts, /*#__PURE__*/React.createElement(_wordpress_block_editor__WEBPACK_IMPORTED_MODULE_0__.InspectorControls, null, /*#__PURE__*/React.createElement(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.PanelBody, null, /*#__PURE__*/React.createElement(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.SelectControl, {
-    label: "Selected Petition",
-    value: formId,
-    options: petitionOptions,
-    onChange: function onChange(el) {
-      return setAttributes({
-        formId: el
-      });
-    }
-  }))), /*#__PURE__*/React.createElement(FinalCompontnt, null));
-}
-
-/***/ }),
-
-/***/ "@wordpress/block-editor":
-/*!*************************************!*\
-  !*** external ["wp","blockEditor"] ***!
-  \*************************************/
-/***/ ((module) => {
-
-module.exports = window["wp"]["blockEditor"];
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: /Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/src/gutenberg/form/Edit.jsx: Support for the experimental syntax 'jsx' isn't currently enabled (55:20):\n\n\u001b[0m \u001b[90m 53 |\u001b[39m     \u001b[36mconst\u001b[39m \u001b[33mFinalCompontnt\u001b[39m \u001b[33m=\u001b[39m () \u001b[33m=>\u001b[39m {\n \u001b[90m 54 |\u001b[39m         \u001b[36mif\u001b[39m (\u001b[33m!\u001b[39mallPetitions\u001b[33m.\u001b[39mlength) {\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 55 |\u001b[39m             \u001b[36mreturn\u001b[39m \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m style\u001b[33m=\u001b[39m{\n \u001b[90m    |\u001b[39m                    \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 56 |\u001b[39m                 {\n \u001b[90m 57 |\u001b[39m                     padding\u001b[33m:\u001b[39m \u001b[32m'24px'\u001b[39m\u001b[33m,\u001b[39m\n \u001b[90m 58 |\u001b[39m                     background\u001b[33m:\u001b[39m \u001b[32m'#efefef'\u001b[39m\u001b[0m\n\nAdd @babel/preset-react (https://github.com/babel/babel/tree/main/packages/babel-preset-react) to the 'presets' section of your Babel config to enable transformation.\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://github.com/babel/babel/tree/main/packages/babel-plugin-syntax-jsx) to the 'plugins' section to enable parsing.\n\nIf you already added the plugin for this syntax to your config, it's possible that your config isn't being loaded.\nYou can re-run Babel with the BABEL_SHOW_CONFIG_FOR environment variable to show the loaded configuration:\n\tnpx cross-env BABEL_SHOW_CONFIG_FOR=/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/src/gutenberg/form/Edit.jsx <your build command>\nSee https://babeljs.io/docs/configuration#print-effective-configs for more info.\n\n    at constructor (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:362:19)\n    at Parser.raise (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:3260:19)\n    at Parser.expectOnePlugin (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:3294:18)\n    at Parser.parseExprAtom (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10929:18)\n    at Parser.parseExprSubscripts (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10584:23)\n    at Parser.parseUpdate (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10569:21)\n    at Parser.parseMaybeUnary (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10549:23)\n    at Parser.parseMaybeUnaryOrPrivate (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10403:61)\n    at Parser.parseExprOps (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10408:23)\n    at Parser.parseMaybeConditional (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10385:23)\n    at Parser.parseMaybeAssign (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10348:21)\n    at Parser.parseExpressionBase (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10302:23)\n    at /Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10298:39\n    at Parser.allowInAnd (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:11928:16)\n    at Parser.parseExpression (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10298:17)\n    at Parser.parseReturnStatement (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12618:28)\n    at Parser.parseStatementContent (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12270:21)\n    at Parser.parseStatementLike (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12239:17)\n    at Parser.parseStatementListItem (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12219:17)\n    at Parser.parseBlockOrModuleBlockBody (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12792:61)\n    at Parser.parseBlockBody (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12785:10)\n    at Parser.parseBlock (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12773:10)\n    at Parser.parseStatementContent (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12329:21)\n    at Parser.parseStatementLike (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12239:17)\n    at Parser.parseStatementOrSloppyAnnexBFunctionDeclaration (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12229:17)\n    at Parser.parseIfStatement (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12606:28)\n    at Parser.parseStatementContent (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12268:21)\n    at Parser.parseStatementLike (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12239:17)\n    at Parser.parseStatementListItem (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12219:17)\n    at Parser.parseBlockOrModuleBlockBody (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12792:61)\n    at Parser.parseBlockBody (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12785:10)\n    at Parser.parseBlock (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12773:10)\n    at Parser.parseFunctionBody (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:11618:24)\n    at Parser.parseArrowExpression (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:11593:10)\n    at Parser.parseParenAndDistinguishExpression (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:11207:12)\n    at Parser.parseExprAtom (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10851:23)\n    at Parser.parseExprSubscripts (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10584:23)\n    at Parser.parseUpdate (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10569:21)\n    at Parser.parseMaybeUnary (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10549:23)\n    at Parser.parseMaybeUnaryOrPrivate (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10403:61)\n    at Parser.parseExprOps (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10408:23)\n    at Parser.parseMaybeConditional (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10385:23)\n    at Parser.parseMaybeAssign (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10348:21)\n    at /Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10318:39\n    at Parser.allowInAnd (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:11928:16)\n    at Parser.parseMaybeAssignAllowIn (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:10318:17)\n    at Parser.parseVar (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12860:91)\n    at Parser.parseVarStatement (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12706:10)\n    at Parser.parseStatementContent (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12322:23)\n    at Parser.parseStatementLike (/Users/antonpersonal/Local Sites/petitions/app/public/wp-content/plugins/petitioner/node_modules/@babel/parser/lib/index.js:12239:17)");
 
 /***/ }),
 
@@ -110,27 +17,8 @@ module.exports = window["wp"]["blockEditor"];
   \********************************/
 /***/ ((module) => {
 
+"use strict";
 module.exports = window["wp"]["blocks"];
-
-/***/ }),
-
-/***/ "@wordpress/components":
-/*!************************************!*\
-  !*** external ["wp","components"] ***!
-  \************************************/
-/***/ ((module) => {
-
-module.exports = window["wp"]["components"];
-
-/***/ }),
-
-/***/ "@wordpress/data":
-/*!******************************!*\
-  !*** external ["wp","data"] ***!
-  \******************************/
-/***/ ((module) => {
-
-module.exports = window["wp"]["data"];
 
 /***/ }),
 
@@ -140,17 +28,8 @@ module.exports = window["wp"]["data"];
   \******************************/
 /***/ ((module) => {
 
+"use strict";
 module.exports = window["wp"]["i18n"];
-
-/***/ }),
-
-/***/ "@wordpress/server-side-render":
-/*!******************************************!*\
-  !*** external ["wp","serverSideRender"] ***!
-  \******************************************/
-/***/ ((module) => {
-
-module.exports = window["wp"]["serverSideRender"];
 
 /***/ })
 
@@ -223,6 +102,9 @@ module.exports = window["wp"]["serverSideRender"];
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
 /*!*************************************!*\
   !*** ./src/gutenberg/form/index.js ***!
   \*************************************/
@@ -259,10 +141,10 @@ __webpack_require__.r(__webpack_exports__);
   category: 'widgets',
   edit: _Edit_jsx__WEBPACK_IMPORTED_MODULE_2__["default"],
   // React component for the editor view
-  save: function save() {
-    return null;
-  } // React component for the frontend view
+  save: () => null // React component for the frontend view
 });
+})();
+
 /******/ })()
 ;
 //# sourceMappingURL=petitionerFormBlock.js.map
