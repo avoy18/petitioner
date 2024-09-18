45b55922ece223d32a9a1975bd143513237bd9b7
Anton Voytenko <avoy18@gmail.com>
2024-09-18 17:27:08 -0400

    update version

--- dist/gutenberg/petitionerFormBlock.asset.php
+++ dist/gutenberg/petitionerFormBlock.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-i18n'), 'version' => '8f386837b074758e327e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => '9d8ec74f3c82dd6feaea');
--- petitioner.php
+++ petitioner.php
@@ -5,7 +5,7 @@
  * Description:       A WordPress plugin for collecting petitions.
  * Requires at least: 5.9
  * Requires PHP:      8.0
- * Version:           0.1.0
+ * Version:           0.1.1
  * Author:            Anton Voytenko
  * License:           GPLv2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
@@ -14,7 +14,7 @@
 
 define('PTR_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
-define('PTR_ASSET_VERSION', '0.1.0');
+define('PTR_ASSET_VERSION', '0.1.1');
 
 require_once PTR_PLUGIN_DIR . 'inc/class-petitioner-mailer.php';
 require_once PTR_PLUGIN_DIR . 'inc/class-petitioner-frontend.php';
