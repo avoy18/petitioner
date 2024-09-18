2beb5dc5bd926ca529803cdb7c04e80d3ee45bc7
Anton Voytenko <avoy18@gmail.com>
2024-09-18 17:46:23 -0400

    minify and deploy

--- dist/gutenberg/petitionerFormBlock.asset.php
+++ dist/gutenberg/petitionerFormBlock.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => 'bd136102073bdf530064');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-i18n', 'wp-server-side-render'), 'version' => 'f07b0ccbf89d01f7bbbf');
