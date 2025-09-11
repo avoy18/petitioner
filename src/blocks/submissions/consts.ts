d3c32e03626765bed95dbad5960ff5588928beb3
Anton Voytenko <avoy18@gmail.com>
2025-09-10 22:27:57 -0400

    improve the dropdown and allow more fields to be selected in GB

--- src/blocks/submissions/consts.ts
+++ src/blocks/submissions/consts.ts
@@ -1,15 +1,19 @@
+import { TokenItem } from '@wordpress/components/build-types/form-token-field/types';
+
+export type FieldType = string | TokenItem;
+
 export type PetitionFormBlockAttributes = {
-    formId: string;
-    newPetitionLink?: string;
-    perPage: number;
-    style: 'simple' | 'table';
-    fields: string[];
-    showPagination: boolean;
-    availableFields: string[];
-    availableStyles: string[];
+	formId: string;
+	newPetitionLink?: string;
+	perPage: number;
+	style: 'simple' | 'table';
+	fields: FieldType[];
+	showPagination: boolean;
+	availableFields: string[];
+	availableStyles: string[];
 };
 
 export type PetitionerSubmissionsProps = {
-    attributes: PetitionFormBlockAttributes;
-    setAttributes: (attrs: Partial<PetitionFormBlockAttributes>) => void;
+	attributes: PetitionFormBlockAttributes;
+	setAttributes: (attrs: Partial<PetitionFormBlockAttributes>) => void;
 };
