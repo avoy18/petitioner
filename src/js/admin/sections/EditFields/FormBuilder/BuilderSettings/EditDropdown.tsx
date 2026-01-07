c661f1306c90e6161a52da9076a2f34af0a0314b
Anton Voytenko <avoy18@gmail.com>
2026-01-07 16:40:53 -0500

    add country settings mvp

--- src/js/admin/sections/EditFields/FormBuilder/BuilderSettings/EditDropdown.tsx
+++ src/js/admin/sections/EditFields/FormBuilder/BuilderSettings/EditDropdown.tsx
@@ -1,6 +1,52 @@
 import { useFormBuilderContext } from '@admin/context/FormBuilderContext';
-import { TextControl, CheckboxControl } from '@wordpress/components';
+import type { BuilderField } from '../consts';
+import {
+	TextControl,
+	CheckboxControl,
+	SelectControl,
+	ToggleControl,
+} from '@wordpress/components';
 import { useState } from '@wordpress/element';
+import { useEditFormContext } from '@admin/context/EditFormContext';
+import { __ } from '@wordpress/i18n';
+import { FormTokenField } from '@wordpress/components';
+
+const CountrySettings = () => {
+	const { formState } = useEditFormContext();
+	const { formBuilderFields, updateFormBuilderFields, builderEditScreen } =
+		useFormBuilderContext();
+	const countryList = formState.default_values?.country_list || [];
+
+	return (
+		<>
+			<FormTokenField
+				label={__('Manually select countries to show', 'petitioner')}
+				value={formBuilderFields[builderEditScreen].country_list || []}
+				suggestions={countryList}
+				onChange={(tokens) =>
+					updateFormBuilderFields(builderEditScreen, {
+						...formBuilderFields[builderEditScreen],
+						country_list: tokens,
+					})
+				}
+			/>
+		</>
+	);
+};
+
+const AdditionalSettings = ({
+	builderEditScreen,
+	draftLabelValue,
+}: {
+	builderEditScreen: string;
+	draftLabelValue: BuilderField['label'];
+}) => {
+	if (builderEditScreen === 'country') {
+		return <CountrySettings />;
+	}
+
+	return null;
+};
 
 export default function EditDropdown() {
 	const { formBuilderFields, updateFormBuilderFields, builderEditScreen } =
@@ -46,6 +92,13 @@ export default function EditDropdown() {
 					onChange={onRequiredEditComplete}
 				/>
 			</p>
+
+			{AdditionalSettings && (
+				<AdditionalSettings
+					builderEditScreen={builderEditScreen}
+					draftLabelValue={draftLabelValue}
+				/>
+			)}
 		</div>
 	);
 }
