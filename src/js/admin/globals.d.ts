b523acb5a18f6cea67da49bcb3882c19ce52d2b3
Anton Voytenko <avoy18@gmail.com>
2026-01-14 19:43:39 -0500

    add basic global type for tinymce

--- src/js/admin/globals.d.ts
+++ src/js/admin/globals.d.ts
@@ -1,1 +1,8 @@
 declare const ajaxurl: string;
+declare const tinymce: {
+	init: (options: any) => void;
+	Editor: {
+        remove: () => void;
+        setContent: (content: string) => void;
+    };
+};
