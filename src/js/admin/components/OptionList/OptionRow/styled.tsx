1d72476ac7aabc6b65b34f2cb895e42c51e5c88d
Anton Voytenko <avoy18@gmail.com>
2026-01-17 17:37:16 -0500

    remove styled component

--- src/js/admin/components/OptionList/OptionRow/styled.tsx
+++ src/js/admin/components/OptionList/OptionRow/styled.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { COLORS, FONT_SIZES, SPACINGS, TRANSITIONS } from '@admin/theme';
+import { FONT_SIZES, SPACINGS, TRANSITIONS } from '@admin/theme';
 
 export const Row = styled.tr<{ $isDragging?: boolean }>`
 	opacity: ${({ $isDragging }) => ($isDragging ? 0.5 : 1)};
@@ -16,19 +16,6 @@ export const DragCell = styled.td`
 	.ptr-drag-handle {
 		opacity: 0;
 		transition: opacity ${TRANSITIONS.sm};
-	}
-`;
-
-export const ToggleCell = styled.td`
-	width: 48px;
-	padding: ${SPACINGS.sm};
-
-	.components-toggle-control {
-		margin-bottom: 0;
-	}
-
-	.components-toggle-control__label {
-		display: none;
 	}
 `;
 
