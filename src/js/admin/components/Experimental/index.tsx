a9c909e54f8b4b94361b57fcdb9c8833065a549a
Anton Voytenko <avoy18@gmail.com>
2026-01-17 20:58:34 -0500

    create fallbacks for experimental elements

---
+++ src/js/admin/components/Experimental/index.tsx
@@ -0,0 +1,13 @@
+import type { ReactNode, CSSProperties } from 'react';
+import * as WPComponents from '@wordpress/components';
+
+type BaseProps = { children?: ReactNode; style?: CSSProperties;[key: string]: any };
+
+export const Text = (WPComponents as any).__experimentalText
+    ?? (({ children, ...props }: BaseProps) => <span {...props}>{children}</span>);
+
+export const Heading = (WPComponents as any).__experimentalHeading
+    ?? (({ children, ...props }: BaseProps) => <h2 {...props}>{children}</h2>);
+
+export const Divider = (WPComponents as any).__experimentalDivider
+    ?? ((props: BaseProps) => <hr {...props} style={{ margin: '16px 0', ...props?.style }} />);
