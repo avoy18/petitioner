841500e397b83ced2ac8357470811bd55251658e
Anton Voytenko <avoy18@gmail.com>
2026-01-17 21:13:43 -0500

    dont add styles twice

--- src/js/admin/components/Experimental/index.tsx
+++ src/js/admin/components/Experimental/index.tsx
@@ -11,4 +11,6 @@ export const Heading = (WPComponents as any).__experimentalHeading
     ?? (({ children, ...props }: BaseProps) => <h2 {...props}>{children}</h2>);
 
 export const Divider = (WPComponents as any).__experimentalDivider
-    ?? ((props: BaseProps) => <hr {...props} style={{ margin: `${SPACINGS.lg} 0`, ...props?.style }} />);
+    ?? (({ style, ...props }: BaseProps) => (
+        <hr {...props} style={{ margin: `${SPACINGS.lg} 0`, ...style }} />
+    ));
