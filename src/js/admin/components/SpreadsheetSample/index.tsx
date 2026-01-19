c796f57574c4a7d1a3ec39eb51e1233f4f6660e8
Anton Voytenko <avoy18@gmail.com>
2026-01-18 19:07:09 -0500

    add loading state

--- src/js/admin/components/SpreadsheetSample/index.tsx
+++ src/js/admin/components/SpreadsheetSample/index.tsx
@@ -1,31 +1,55 @@
 import { Table, TableHead, TableBody, TableRow, TableCell, TableHeadCell, TableWrapper } from './styled';
 import type { SpreadsheetSampleProps } from './consts';
+import { useMemo } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 
-export default function SpreadsheetSample({ headings, rows, isLoading = false }: SpreadsheetSampleProps) {
+export default function SpreadsheetSample({ headings, rows, isLoading }: SpreadsheetSampleProps) {
+    const Headings = useMemo(() => {
+        return <>
+            <TableHeadCell></TableHeadCell>
+            {headings.map((heading) => (
+                <TableHeadCell key={heading}>{heading}</TableHeadCell>
+            ))}
+        </>
+    }, [isLoading, headings]);
+
+    const Rows = useMemo(() => {
+        if (rows.length === 0) {
+            return <TableRow>
+                <TableCell colSpan={headings.length}>{__('No data', 'petitioner')}</TableCell>
+            </TableRow>
+        }
+
+        return rows.map((row, index) => (
+            <TableRow key={row.join(',')}>
+                <TableCell>{index + 1}</TableCell>
+                {row.map((cell) => (
+                    <TableCell key={cell}>{cell}</TableCell>
+                ))}
+            </TableRow>
+        ))
+    }, [isLoading, rows, headings]);
+
     if (isLoading) {
-        return 'Loading';
+        return <TableWrapper>
+            <Table>
+                <TableHead>
+                    <TableHeadCell>{__('Loading...', 'petitioner')}</TableHeadCell>
+                </TableHead>
+            </Table>
+        </TableWrapper>
     }
-    
+
     return (
         <TableWrapper>
             <Table>
                 <TableHead>
                     <TableRow>
-                        <TableHeadCell></TableHeadCell>
-                        {headings.map((heading) => (
-                            <TableHeadCell key={heading}>{heading}</TableHeadCell>
-                        ))}
+                        {Headings}
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row, index) => (
-                        <TableRow key={row.join(',')}>
-                            <TableCell>{index + 1}</TableCell>
-                            {row.map((cell) => (
-                                <TableCell key={cell}>{cell}</TableCell>
-                            ))}
-                        </TableRow>
-                    ))}
+                    {Rows}
                 </TableBody>
             </Table>
         </TableWrapper>
