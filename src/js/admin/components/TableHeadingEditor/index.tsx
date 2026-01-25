7495c03c98eb37c6761131f51cc09402d4d68e72
Anton Voytenko <avoy18@gmail.com>
2026-01-24 23:02:30 -0500

    add action buttons

--- src/js/admin/components/TableHeadingEditor/index.tsx
+++ src/js/admin/components/TableHeadingEditor/index.tsx
@@ -1,18 +1,60 @@
 import { useState } from '@wordpress/element';
-import { TableHeadingEditorContainer, TableHeading } from './styled';
+import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
+import { TableHeadingEditorContainer, TableHeading, TableHeadingActions } from './styled';
 import type { TableHeadingEditorProps, Heading } from './consts';
 
 export default function TableHeadingEditor({ headings }: TableHeadingEditorProps) {
 	const [activeHeading, setActiveHeading] = useState<Heading | null>(null);
+	const [deletedHeadings, setDeletedHeadings] = useState<Set<string>>(new Set());
+
+	const handleEditHeading = (id: string) => {
+		setActiveHeading(headings.find((heading) => heading.id === id) || null);
+	};
+
+	const handleDeleteHeading = (id: string) => {
+		setActiveHeading(null);
+		setDeletedHeadings(prev => new Set(prev).add(id));
+	};
+
+	const handleRestoreHeading = (id: string) => {
+		setDeletedHeadings(prev => {
+			const newSet = new Set(prev);
+			newSet.delete(id);
+			return newSet;
+		});
+	};
 
 	return (
 		<TableHeadingEditorContainer>
-			{headings.map((heading) => (
-				<TableHeading
-					key={heading.id}>
-					{heading.label}
-				</TableHeading>
-			))}
+			{headings.map((heading) => {
+				const isDeleted = deletedHeadings.has(heading.id);
+				return (
+					<TableHeading
+						$deleted={isDeleted}
+						key={heading.id}>
+
+						<span>{heading.label}</span>
+
+						<TableHeadingActions>
+							<Button size="small" icon="edit" variant="tertiary" onClick={() => handleEditHeading(heading.id)}></Button>
+							{isDeleted ?
+								<Button
+									size="small"
+									icon="plus"
+									variant="tertiary"
+									onClick={() => handleRestoreHeading(heading.id)} /> :
+								<Button
+									size="small"
+									icon="trash"
+									variant="tertiary"
+									isDestructive={true}
+									onClick={() => handleDeleteHeading(heading.id)}
+								/>}
+						</TableHeadingActions>
+					</TableHeading>
+				)
+			})}
 		</TableHeadingEditorContainer>
 	);
 }
--- src/js/admin/components/TableHeadingEditor/styled.tsx
+++ src/js/admin/components/TableHeadingEditor/styled.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { COLORS, SPACINGS } from '@admin/theme';
 
 export const TableHeadingEditorContainer = styled.div`
@@ -9,9 +9,11 @@ export const TableHeadingEditorContainer = styled.div`
     max-width: 100%;
 `;
 
-export const TableHeading = styled.div<{ $isActive?: boolean }>`
+export const TableHeading = styled.div<{ $isActive?: boolean, $deleted?: boolean }>`
     display: flex;
     flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
     gap: ${SPACINGS.sm};
     padding: ${SPACINGS.sm};
     border: 1px solid ${({ $isActive }) => $isActive ? COLORS.primary : COLORS.grey};
@@ -20,4 +22,17 @@ export const TableHeading = styled.div<{ $isActive?: boolean }>`
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+    
+    > span {
+        ${({ $deleted }) => $deleted && css`
+            opacity: 0.5;
+            text-decoration: line-through;
+        `}
+    }
 `
+
+export const TableHeadingActions = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: ${SPACINGS.xs};
+`;
