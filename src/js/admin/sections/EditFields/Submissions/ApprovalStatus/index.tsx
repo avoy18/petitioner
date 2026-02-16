a3feef1e75a94756a2c396700f20a1c8ab6817c5
Anton Voytenko <avoy18@gmail.com>
2026-02-16 11:48:49 -0500

    chore: remove unused import

--- src/js/admin/sections/EditFields/Submissions/ApprovalStatus/index.tsx
+++ src/js/admin/sections/EditFields/Submissions/ApprovalStatus/index.tsx
@@ -1,6 +1,5 @@
 import { Button } from '@wordpress/components';
 import type { ApprovalStatusProps } from '../consts';
-import ResendButton from './ResendButton';
 
 export default function ApprovalStatus(props: ApprovalStatusProps) {
 	const { item, onStatusChange = () => {}, defaultApprovalState } = props;
