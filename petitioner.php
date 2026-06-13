0c7fa80a7f912fafd0c8e6462567f2600c8ec2b2
Anton Voytenko <avoy18@gmail.com>
2026-06-13 13:05:31 -0400

    fix: Check if file exists just in case

--- petitioner.php
+++ petitioner.php
@@ -36,7 +36,10 @@ if (!function_exists('av_ptr_error_log')) {
     }
 }
 
-require_once AV_PETITIONER_PLUGIN_DIR . 'vendor/woocommerce/action-scheduler/action-scheduler.php';
+if (file_exists(AV_PETITIONER_PLUGIN_DIR . 'vendor/woocommerce/action-scheduler/action-scheduler.php')) {
+    require_once AV_PETITIONER_PLUGIN_DIR . 'vendor/woocommerce/action-scheduler/action-scheduler.php';
+}
+
 require_once AV_PETITIONER_PLUGIN_DIR . 'inc/class-queue.php';
 require_once AV_PETITIONER_PLUGIN_DIR . 'inc/submissions/class-submissions-model.php';
 require_once AV_PETITIONER_PLUGIN_DIR . 'inc/submissions/class-submissions-controller.php';
